b51d6018d790769a5782a303aea39b9ce5a5fb11
zte\0216000158@GDFA0004041-2 <zwd@local.com>
2023-02-22 17:58:44 +0800

    优化层级打印

--- 01-HelloWorld-基本结构/03-类的继承关系.py
+++ 01-HelloWorld-基本结构/03-类的继承关系.py
@@ -1,12 +1,12 @@
 from PySide6 import QtWidgets
 
 
-def get_sub_classes(class_):
+def get_sub_classes(class_, prefix="|--"):
     """递归地显示某一类的所有子类"""
     for subclass in class_.__subclasses__():
-        print(subclass)
+        print(prefix, subclass)
         if len(class_.__subclasses__()) > 0:
-            get_sub_classes(subclass)
+            get_sub_classes(subclass, prefix+"--")
 
 
 # 查看QAbstractButton抽象基类的所有子类，了解Qt6提供了哪些按钮控件
